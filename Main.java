889f5e7066e4b21526472139cf33a978c1bf46c0
Daniel <dte.biotech46@gmail.com>
2026-01-20 12:09:03 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
-/*
-Purpose: to call the other functions and label the analysis sections
-Parameters, Return: none
-*/
 public class Main {
+    /*
+    Purpose: to call the other functions and label the analysis sections
+    Parameters, Return: none
+    */
     public static void main(String[] args) {
         // TBD
+    }
+
+    /*
+    Purpose: to read in the CSV dataset and transform it into a list of lists
+    */
+    static void myMethod() {
+        for (int i = 0; i < 5; i++) {
+            int num = i;
+
+            if (num == 3) {
+                System.out.println("Hooray!");
+            }
+        }
     }
 }
